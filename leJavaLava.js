a924edeafe36dd33e46adbcd0e546cd5d0bb87f0
pspenc <spencerpatrickd@gmail.com>
2018-09-21 00:31:43 +0000

    Hey look, it parses stuff. Like its, totally, Parsival

---
+++ leJavaLava.js
@@ -0,0 +1,46 @@
+
+
+
+function freakingRead(pathOfFileToReadFrom){
+        var request = new XMLHttpRequest();
+        request.open("GET", pathOfFileToReadFrom, false);
+        request.send(null);
+        var returnValue = request.responseText;
+
+        return returnValue;
+}
+
+
+function startPlaylistin(event){
+    
+    
+    
+    var name = document.getElementById("nameForm").value;
+    document.getElementById("peepsName").innerHTML = name;
+    
+    var songList = freakingRead("Songs.txt");
+    var songArray = songList.split("\n");
+    
+    var playlist = [];
+    for (var i = 0;i<name.length;i++){
+        
+
+        var upperLetter = name.charAt(i).toUpperCase();
+        var index = (upperLetter.charCodeAt(0) -65) * 2;
+        
+        
+        var text = songArray[index]+"<br>";
+        playlist.push(text);
+        //alert(songArray[index]);
+        
+    }
+    
+    for(var i = 0; i < playlist.length; i++){
+        
+        document.getElementById("playlistSpot").innerHTML += playlist[i]+"<br>" ;
+        
+    }
+    
+    
+    event.preventDefault();
+}
